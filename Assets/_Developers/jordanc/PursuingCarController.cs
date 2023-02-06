b76b88b6bcd79fcd19da40929dcaf60ebc1f4c5d
ElementalPrism <114480418+ElementalPrism@users.noreply.github.com>
2023-02-06 15:14:35 +0000

    implemented Aggro range on police car
    Added aggro range onto police car as well as some sort of patrol point system with help from joseph.

--- Assets/_Developers/jordanc/PursuingCarController.cs
+++ Assets/_Developers/jordanc/PursuingCarController.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class PursuingCarController : AICarController
 {
-    protected enum State {PURSUE, TURNLEFT, TURNRIGHT, BRAKE}
+    protected enum State { PURSUE, TURNLEFT, TURNRIGHT, BRAKE, PATROL }
     [SerializeField] protected State NextState;
     private AITestCar CurrentCar;
     public CollisionPrevention PreventionCollision;
+    public GameObject Target;
+
+
+
+    [Header("Aggro Range")]
+    [SerializeField] float AggroRange;
+    [SerializeField] LayerMask Car;
+
+    [Header("Patrol Points")]
+    [SerializeField] Vector3[] ListOfPatrolPoints;
+    int NextPatrolPoint;
+    [SerializeField] float DistanceFromPatrolPoint;
 
     protected override void Start()
     {
@@ -17,23 +29,57 @@ public class PursuingCarController : AICarController
 
     protected override void Evaluate()
     {
-      if (PreventionCollision.TurnLeftBoolPass == true)
+        if (PreventionCollision.TurnLeftBoolPass == true)
         {
-           NextState = State.TURNLEFT;
+            NextState = State.TURNLEFT;
         }
-      if (PreventionCollision.TurnRightBoolPass == true) 
+        if (PreventionCollision.TurnRightBoolPass == true)
         {
-            NextState= State.TURNRIGHT;
+            NextState = State.TURNRIGHT;
         }
-      if (PreventionCollision.BrakeBoolPass == true) 
+        if (PreventionCollision.BrakeBoolPass == true)
         {
-            NextState= State.BRAKE;
+            NextState = State.BRAKE;
         }
 
-      if ((PreventionCollision.TurnLeftBoolPass == false) && (PreventionCollision.TurnRightBoolPass == false) && (PreventionCollision.BrakeBoolPass == false))
+        /*
+        if ((PreventionCollision.TurnLeftBoolPass == false) && (PreventionCollision.TurnRightBoolPass == false) && (PreventionCollision.BrakeBoolPass == false))
         {
             NextState = State.PURSUE;
         }
+        */
+
+        RaycastHit[] Hits = Physics.SphereCastAll(transform.position, AggroRange, Vector3.forward, 0, Car);
+
+        if (Hits.Length > 0)
+        {
+            foreach (RaycastHit hit in Hits)
+            {
+                if (hit.transform.gameObject != gameObject && hit.transform.CompareTag("Player"))
+                {
+                    
+                    Target = hit.transform.gameObject;
+
+
+                }
+            }
+
+
+        }
+
+        if (Target != null) 
+        {
+            if (Vector3.Distance(agent.transform.position, Target.transform.position) <= AggroRange)
+            {
+                NextState = State.PURSUE;
+            }
+            else
+            {
+                NextState = State.PATROL;
+                Target = null;
+            }
+        }
+
 
     }
 
@@ -41,11 +87,14 @@ public class PursuingCarController : AICarController
 
     protected override void SwapState()
     {
-      
-        switch(NextState)
+
+        switch (NextState)
         {
             case State.PURSUE:
                 Pursue();
+                break;
+            case State.PATROL:
+                Patrol();
                 break;
             case State.TURNLEFT:
                 TurnLeft();
@@ -54,7 +103,7 @@ public class PursuingCarController : AICarController
                 TurnRight();
                 break;
             case State.BRAKE:
-                Brake(); 
+                Brake();
                 break;
         }
     }
@@ -63,20 +112,22 @@ public class PursuingCarController : AICarController
 
     protected override void Act()
     {
-        if (NextState == State.PURSUE) 
-        {  
+        if (NextState == State.PURSUE || NextState == State.PATROL)
+        {
             FollowAgent();
         }
-       
+
 
         State c = NextState;
 
         Evaluate();
 
+        /*
         if (Input.GetKeyDown(KeyCode.I) && agentDebug)
         {
             agentDebug.SetActive(!agentDebug.activeInHierarchy);
         }
+        */
 
         if (c != NextState) newState = true;
 
@@ -86,7 +137,11 @@ public class PursuingCarController : AICarController
 
     private void Pursue()
     {
-
+        if(Target != null) 
+        {
+            agent.SetDestination(Target.transform.position); 
+        }
+        
     }
 
     private void TurnLeft()
@@ -104,11 +159,32 @@ public class PursuingCarController : AICarController
         CurrentCar.Accelerate(-1);
     }
 
+    private void Patrol()
+    {
+
+        float DistanceToNext = Vector3.Distance(transform.position, ListOfPatrolPoints[NextPatrolPoint]);
+
+        if (DistanceToNext <= DistanceFromPatrolPoint)
+        {
+            NextPatrolPoint += 1;
+
+            if (NextPatrolPoint >= ListOfPatrolPoints.Length)
+            {
+                NextPatrolPoint = 0;
+            }
+
+        }
+
+        agent.SetDestination(ListOfPatrolPoints[NextPatrolPoint]);
 
 
 
+    }
 
-
-
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color (255,0,0, 255);
+        Gizmos.DrawSphere(transform.position, AggroRange);
+    }
 
 }
