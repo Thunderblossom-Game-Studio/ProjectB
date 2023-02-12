39b31b459886dc745fabe0eb717a0627065c60c9
DENraynaH <103390897+DENraynaH@users.noreply.github.com>
2023-02-12 01:25:29 +0000

    Entity Spawner Updates
    Added a "Grounded" setting which automatically with spawn the entity at ground level.

--- Assets/_Developers/GP/JakeE/EntitySpawner.cs
+++ Assets/_Developers/GP/JakeE/EntitySpawner.cs
@@ -9,8 +9,8 @@ public class EntitySpawner : MonoBehaviour
     public List<SpawnableObject> SpawnedObjects => _spawnedObjects;
 
     #endregion
-    
-    
+
+    [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private Color _spawnZoneColor;
     [SerializeField] private List<Zone> _spawnZones;
     [SerializeField] private List<ObjectType> _objectTypes;
@@ -21,10 +21,10 @@ public class EntitySpawner : MonoBehaviour
         if (_spawnZones.Count <= 0 || _objectTypes.Count <= 0) return;
         Zone randomZone = _spawnZones[Random.Range(0, _spawnZones.Count)];
         Vector3 randomPosition = GetRandomPosition(randomZone);
-        GameObject spawnedObject = Instantiate(GetObject().Object, randomPosition, Quaternion.identity);
+        Vector3 spawnPosition = ConvertToGroundPosition(randomZone, randomPosition);
+        GameObject spawnedObject = Instantiate(GetObject().Object, spawnPosition, Quaternion.identity);
         spawnedObject.AddComponent<SpawnableObject>().Initialise(this);
     }
-
     private Vector3 GetRandomPosition(Zone randomZone)
     {
         return new Vector3(randomZone.Position.x + Random.Range(-randomZone.Size.x / 2, randomZone.Size.x / 2),
@@ -59,6 +59,12 @@ public class EntitySpawner : MonoBehaviour
             weightList.Add(currentWeight);
         }
         return weightList;
+    }
+
+    private Vector3 ConvertToGroundPosition(Zone selectedZone, Vector3 randomPosition)
+    {
+        return Physics.Raycast(new Vector3(randomPosition.x, selectedZone.Position.y + selectedZone.Size.y / 2, randomPosition.z),
+            Vector3.down, out var raycastHit, selectedZone.Size.y * 2, _groundLayer) ? raycastHit.point : randomPosition;
     }
 
     private void OnDrawGizmos()
