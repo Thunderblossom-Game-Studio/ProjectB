a54dce75f40d5d784fcbd2acd37d7001f4f3e749
Jack HK <jackhughesking@gmail.com>
2023-02-17 20:55:15 +0000

    Added GizmoDisk
    added wire disk and solid disk gizmos

---
+++ Assets/_Developers/GP/JackHK/Scripts/AOEHazard.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AOEHazard : MonoBehaviour
+{ 
+    //create AOEHazard radius (circle)
+
+    //create hazards zone sizes
+
+    //timer OR proximity setting
+}
