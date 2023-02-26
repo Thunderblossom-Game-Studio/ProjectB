b6b414675c495ed7f5b922389bed63cbf09b9ba5
AntonN2001 <anton.narva.2001@gmail.com>
2023-02-26 21:33:41 +0000

    Blimp stops when dropping bombs

--- Assets/_Developers/GP/AntonN/Scripts/Blimp.cs
+++ Assets/_Developers/GP/AntonN/Scripts/Blimp.cs
@@ -7,38 +7,29 @@ public class Blimp : MonoBehaviour
     [SerializeField] private GameObject droppableObject;
     [SerializeField] private Transform dropPoint;
     [SerializeField] private float dropRate = 0.1f;
-    //[SerializeField] private Transform pointA;
-    //[SerializeField] private Transform pointB;
-    //[SerializeField] private Transform pointC;
-    //[SerializeField] private Transform pointD;
-    //[SerializeField] private Transform pointABCD;
+    [SerializeField] private RouteUser ru;
+    [SerializeField] private Transform target;
+
 
     private float interpolateAmount;
+
     private bool blimpIsMoving;
     private bool playerDetected;
-    private float nextTimeToAttack = 2;
-    private bool indicatorSwitch;
+    private float nextTimeToAttack = 6f;
 
     [SerializeField] private float detectionHeight;
 
+
     private void Start()
     {
+        ru.Activate(0);
         blimpIsMoving = true;
         playerDetected = false;
-        indicatorSwitch = false;
     }
 
     private void Update()
     {
-        //interpolateAmount = (interpolateAmount + Time.deltaTime) % 1f;
-        //pointABCD.position = CubicLerp(pointA.position, pointB.position, pointC.position, pointD.position, interpolateAmount);
-
         CheckForPlayer();
-
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            SpawnIndicator();
-        }
     }
 
     private void FixedUpdate()
@@ -48,7 +39,6 @@ public class Blimp : MonoBehaviour
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, layerMask))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.red);
-
             playerDetected = true;
             blimpIsMoving = false;
         }
@@ -57,48 +47,29 @@ public class Blimp : MonoBehaviour
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * detectionHeight, Color.blue);
             playerDetected = false;
             blimpIsMoving = true;
+            ru.ToggleMovement(true);
         }
     }
-
-    /*private Vector3 QuadraticLerp(Vector3 a, Vector3 b, Vector3 c, float t)
-    {
-        Vector3 ab = Vector3.Lerp(a, b, t);
-        Vector3 bc = Vector3.Lerp(b, c, t);
-        return Vector3.Lerp(ab, bc, interpolateAmount);
-    }
-
-    private Vector3 CubicLerp(Vector3 a, Vector3 b, Vector3 c, Vector3 d, float t)
-    {
-        Vector3 ab_bc = QuadraticLerp(a, b, c, t);
-        Vector3 bc_cd = QuadraticLerp(b, c, d, t);
-        return Vector3.Lerp(ab_bc, bc_cd, interpolateAmount);
-    }*/
 
     private void CheckForPlayer()
     {
        if(playerDetected)
         {
-            
-            
+
             if (Time.time > nextTimeToAttack)
             {
+                ru.Activate(0, false);
                 nextTimeToAttack = Time.time + 1 / dropRate;
-                SpawnIndicator();
-                
+                DropBomb();
             }
         }
-    }
-
-    private void SpawnIndicator()
-    {
-        
-        
-        DropBomb();
     }
     
     private void DropBomb()
     {
-       Debug.Log("Bomb!");
-       Instantiate(droppableObject, dropPoint.position, Quaternion.identity);
+        Debug.Log("Bomb!");
+        Instantiate(droppableObject, dropPoint.position, Quaternion.identity);
     }
+
+
 }
