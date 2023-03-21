319c1c17c17bb0745d2b1c26b723656361e92b3a
Joseph <jwlivingstone2002@gmail.com>
2023-03-21 19:00:25 +0000

    Prefabed versions of level 2 and 3 traffic

--- Assets/_Developers/AI/timjm/WaypointNaME.cs
+++ Assets/_Developers/AI/timjm/WaypointNaME.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class WaypointNaME : MonoBehaviour
 {
     public Transform t;
+
+    public bool showText = false;
+
     // Start is called before the first frame update
     void OnDrawGizmos()
     {
-        TextMesh textMesh = t.GetComponent(typeof(TextMesh)) as TextMesh;
-        textMesh.text = transform.name;
+
+        //TextMesh textMesh = t.GetComponent(typeof(TextMesh)) as TextMesh;
+        //textMesh.text = transform.name;
 
     }
 }
