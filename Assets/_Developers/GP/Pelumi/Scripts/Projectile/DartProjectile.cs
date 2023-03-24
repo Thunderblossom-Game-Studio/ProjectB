b32a965ca0411ae8d64b9e05c4f43a43b0ced747
Oluwapelumi Ayeni <noluwapelumi4@gmail.com>
2023-03-24 00:41:09 +0000

    scyncing weapon to multiplayer

--- Assets/_Developers/GP/Pelumi/Scripts/Projectile/DartProjectile.cs
+++ Assets/_Developers/GP/Pelumi/Scripts/Projectile/DartProjectile.cs
@@ -1,3 +1,4 @@
+using JE.DamageSystem;
 using JE.General;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,15 +7,22 @@ using UnityEngine;
 public class DartProjectile : Projectile
 {
     [SerializeField] protected float destroyTime;
+    [SerializeField] protected float damageRadius;
+    [SerializeField] protected float _damageAmount;
+    [SerializeField] private int _hitEntitiesMaximum = 10;
+    private bool hasHit = false;
+    private Collider[] _hitEntities;
 
     protected void Start()
     {
+        _hitEntities = new Collider[_hitEntitiesMaximum];
         StartCoroutine(LifeTimeDelay());
     }
 
     private void Update()
     {
         rb.velocity = transform.forward * speed;
+        DetectDamageable();
     }
 
     public void SetUp(float _speed)
@@ -40,9 +48,26 @@ public class DartProjectile : Projectile
         base.DestroyProjectile();
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void DetectDamageable()
     {
-        if (!detectLayer.ContainsLayer(other.gameObject.layer)) return;
+        if (hasHit) return;
+        int entitiesHit = Physics.OverlapSphereNonAlloc(transform.position , damageRadius, _hitEntities, detectLayer);
+        hasHit = entitiesHit != 0;
+        for (int i = 0; i < entitiesHit; i++) DamageEntity(_hitEntities[i].gameObject);
         DestroyProjectile();
+    }
+
+    protected void DamageEntity(GameObject damageObject)
+    {
+        IDamageable healthSystem = damageObject.GetComponent<IDamageable>();
+        if (healthSystem == null) return;
+        healthSystem.ReduceHealth(_damageAmount);
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, damageRadius);
     }
 }
