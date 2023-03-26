d67a449911e6840002c55c66a2d8c3b1f96b3374
Paul <62595708+Tucod@users.noreply.github.com>
2023-03-26 21:36:46 +0100

    Revert "Health Bar hooked up to Game Menu"
    This reverts commit f062c653ba5ff57e33be4c161af9a551aa3367ee.

--- Assets/_Developers/AP/Jess/Scripts/HealthBar.cs
+++ Assets/_Developers/AP/Jess/Scripts/HealthBar.cs
@@ -1,4 +1,3 @@
-using JE.DamageSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +6,6 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
-    public HealthSystem healthSystem;
 
     public void SetMaxHealth(int health)
     {
@@ -19,25 +17,5 @@ public class HealthBar : MonoBehaviour
     {
         slider.value = health;
     }
-
-    public void UpdateHealth()
-    {
-        slider.value = healthSystem.CurrentHealth;
-    }
-    ///Used for Debugging
-
-    //private void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.F))
-    //    {
-    //        healthSystem.ReduceHealth(10);
-
-    //    }
-
-    //    if (Input.GetKeyDown(KeyCode.V))
-    //    {
-    //        healthSystem.RestoreHealth(10);
-    //    }
-    //}
-
+        
 }
